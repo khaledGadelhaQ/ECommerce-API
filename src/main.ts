837d57d71d5d9cce2b6531fdcfff6c2e34eb4e10
Khaled Gadelhaq <khaledmogadelhaq@gmial.com>
2024-12-31 22:11:24 +0200

    Implementing Versioning

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // app.enableVersioning({
+  //   type: VersioningType.URI,
+  // });
+  app.setGlobalPrefix('api/v1');
   const port = process.env.PORT;
   await app.listen(port ?? 2000);
-  console.log(`Listing on port: ${port}`);
 }
 bootstrap();
